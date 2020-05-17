ad8b0a22f58deac71a82915e9684bc8221b559b0
Cellphone104 <53287422+dmuller104@users.noreply.github.com>
2020-05-16 23:38:48 -0600

    Introduced myself to Alex

--- src/alex/HelloEu.java
+++ src/alex/HelloEu.java
@@ -2,6 +2,8 @@ package alex;
 
 public class HelloEu {
     public static void main(String[] args ){
+
         System.out.println("Hello, my name is Alex Brasil!");
+        System.out.println("Hello Alex, my name is Derek!");
     }
 }
